eac601b04d0d6f0350743d503e2deb9b4f3592d6
YJammak <yjk@yjk.name>
2026-03-15 22:00:22 +0800

    Add a close prompt and set the default language to English.

--- src/AvaloniaProject/Views/MainWindow.axaml.cs
+++ src/AvaloniaProject/Views/MainWindow.axaml.cs
@@ -1,7 +1,9 @@
 using System.Reactive.Disposables;
 using System.Reactive.Disposables.Fluent;
+using System.Threading.Tasks;
 using AvaloniaProject.ViewModels;
 using ReactiveUI;
+using Ursa.Controls;
 using Ursa.ReactiveUIExtension;
 
 namespace AvaloniaProject.Views;
@@ -22,5 +24,15 @@ public partial class MainWindow : ReactiveUrsaWindow<MainWindowViewModel>
                 vm => vm.MainView,
                 v => v.ViewModelViewHost.ViewModel)
             .DisposeWith(disposable);
+    }
+
+    protected override async Task<bool> CanClose()
+    {
+        var result = await MessageBox.ShowAsync(
+            "Are you sure you want to close?",
+            "Close",
+            MessageBoxIcon.Question,
+            MessageBoxButton.OKCancel);
+        return result == MessageBoxResult.OK;
     }
 }
