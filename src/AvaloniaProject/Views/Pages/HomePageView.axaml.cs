23f24918a1ca0e8fdd280e8745e3cf46e8a599bf
YJammak <yjk@yjk.name>
2026-03-18 14:16:06 +0800

    Display homepage content using Markdown.

--- src/AvaloniaProject/Views/Pages/HomePageView.axaml.cs
+++ src/AvaloniaProject/Views/Pages/HomePageView.axaml.cs
@@ -1,12 +1,33 @@
-﻿using AvaloniaProject.ViewModels.Pages;
+﻿using System.Reactive.Disposables;
+using AvaloniaProject.Services;
+using AvaloniaProject.ViewModels.Pages;
+using LiveMarkdown.Avalonia;
+using ReactiveUI;
 using Ursa.ReactiveUIExtension;
 
 namespace AvaloniaProject.Views.Pages;
 
 public partial class HomePageView : ReactiveUrsaView<HomePageViewModel>
 {
+    private readonly ObservableStringBuilder _markdownBuilder;
+
     public HomePageView()
     {
         InitializeComponent();
+
+        _markdownBuilder = new ObservableStringBuilder();
+        MarkdownRenderer.MarkdownBuilder = _markdownBuilder;
+        UpdateContent();
+        LocalizationService.Instance.CultureChanged += (_, _) => UpdateContent();
+
+        this.WhenActivated(OnWhenActivated);
+    }
+
+    private void OnWhenActivated(CompositeDisposable disposable) { }
+
+    private void UpdateContent()
+    {
+        _markdownBuilder.Clear();
+        _markdownBuilder.AppendLine(LocalizationService.Instance["HomePage_Content"]);
     }
 }
